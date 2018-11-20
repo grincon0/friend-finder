88b63b9b5b25051a18eaff0a7cc2499a8c66e6d5
Wake <georgerincon2@gmail.com>
2018-11-19 22:46:08 -0500

    added matching algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,6 @@
-const friends = require("../data/friends");
+const path = require('path');
+
+const friends = require("../data/friends.js");
 
 module.exports = function(app){
 
@@ -10,9 +12,34 @@ module.exports = function(app){
 
 
 
-    app.post("/api/friends", function(req,res){
-
+    app.post("/api/friends", function(req, res){
+        let userArr = (req.body);
+        let match = [];
+        let totalDiff;
+        let currentDiff = 0;
         
+        for(let i = 0; i < friends.length; i++){
+
+            for(let z = 0; z < userArr.scores.length; z++){
+                
+                if((friends[i].scores[z] - userArr.scores[z]) !== 0){
+
+                    currentDiff += (Math.abs(friends[i].scores[z] - userArr.scores[z]));
+                }
+            }
+
+            if(!totalDiff || totalDiff > currentDiff){
+                match = friends[i].name;
+                totalDiff = currentDiff;
+                currentDiff = 0;
+            }else{
+                currentDiff = 0;
+            }
+        }
+
+        friends.push(userArr);
+        
+        res.json({status:"OK", matched: match, diff: totalDiff});
 
     });
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const app = express();
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 
 require("./app/routing/apiRoutes")(app);
