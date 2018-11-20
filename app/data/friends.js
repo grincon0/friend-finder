cf43aae8ed6d1296cd5b3326b11469de05d6700e
Wake <georgerincon2@gmail.com>
2018-11-19 23:30:33 -0500

    added new friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,6 +7,11 @@ let friends = [
         name: "Lan",
         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores: ['2', '1', '3', '5', '3', '5', '3', '3', '2','2']
+    },{
+        name: "Kylie Jenner",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: ['1', '1', '1', '1', '1', '1', '3', '3', '2','2']
+
     }
 ];
 
